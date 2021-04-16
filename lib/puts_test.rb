b2645d4fb2022dfc4cc612aaae66416c4a0e965b
root <JennaChadwick>
2021-04-16 19:38:46 +0000

    Finished

---
+++ lib/puts_test.rb
@@ -0,0 +1,8 @@
+def say_hi
+    return "hi"
+end
+
+puts "hello world"
+puts 3
+puts [1, 2, 3]
+puts say_hi
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,114 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  for i in arr
+    sum = sum + i
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  num1 = 0
+  num2 = 0
+  arr = arr.sort.reverse
+  if arr[0].is_a? Integer
+    num1 = arr[0]
+  end
+  if arr[1].is_a? Integer
+    num2 = arr[1]
+  end
+  return num1 + num2
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  subtract = 0
+  if arr == []
+    return false
+  end
+  if arr.length == 1
+    return false
+  end
+
+  for i in arr
+    subtract = n - i
+    for j in arr
+      if i == j
+        break
+      end
+      if j == subtract
+        return true
+      end
+    end
+  end
+
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+  
+  if s == []
+    return false
+  end
+
+  first = s[0, 1].downcase
+
+  for letter in consonants
+    if first == letter
+      return true
+    end
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s = s.reverse
+
+  unless s.delete('01') == ''
+    return false
+  end
+
+  if s[0, 2] == '00'
+    return true
+  end
+  if s[0] == '0' and s.length == 1
+    return true
+  end
+  return false
 end
 
 # Part 3
 
+
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(b_i, b_p)
+    if b_i == ''
+      raise ArgumentError
+    end
+    if b_p <= 0
+      raise ArgumentError
+    end
+    @isbn = b_i
+    @price = b_p
+  end
+
+  def price_as_string
+    return "$#{sprintf("%0.02f", @price)}"
+  end
 end
+
+fiction = BookInStock.new("isbn1", 33.6)
+puts(fiction.isbn)
+puts(fiction.price)
+fiction.price = 83.4
+puts(fiction.price)
